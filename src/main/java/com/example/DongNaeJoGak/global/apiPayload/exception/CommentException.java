4e88d324777aacf644123956c3a2081b06231b9e
hyungjun0312 <156741635+hyungjun0312@users.noreply.github.com>
2025-07-01 04:39:59 +0900

    ✨ feat: 댓글 작성 및 신고 기능 구현

---
+++ src/main/java/com/example/DongNaeJoGak/global/apiPayload/exception/CommentException.java
@@ -0,0 +1,8 @@
+package com.example.DongNaeJoGak.global.apiPayload.exception;
+
+import com.example.DongNaeJoGak.global.apiPayload.code.BaseErrorCode;
+
+public class CommentException extends GeneralException {
+
+    public CommentException(BaseErrorCode baseErrorCode) { super(baseErrorCode); }
+}
