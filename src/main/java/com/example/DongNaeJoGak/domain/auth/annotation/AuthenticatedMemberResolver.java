795483eaf3e7066d600de9fb75d427e37644406e
m1nzez <vivi020405@naver.com>
2025-07-05 18:27:27 +0900

    :bug: fix: 인증 안된 사용자 에러 처리

--- src/main/java/com/example/DongNaeJoGak/domain/auth/annotation/AuthenticatedMemberResolver.java
+++ src/main/java/com/example/DongNaeJoGak/domain/auth/annotation/AuthenticatedMemberResolver.java
@@ -1,6 +1,8 @@
 package com.example.DongNaeJoGak.domain.auth.annotation;
 
 import com.example.DongNaeJoGak.domain.member.entity.Member;
+import com.example.DongNaeJoGak.global.apiPayload.code.status.error.TokenErrorStatus;
+import com.example.DongNaeJoGak.global.apiPayload.exception.TokenException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
@@ -30,6 +32,12 @@ public class AuthenticatedMemberResolver implements HandlerMethodArgumentResolve
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         // secuirtyContext에서 인증정보 꺼냄
         Authentication authentication = securityContextRepository.loadDeferredContext(webRequest.getNativeRequest(HttpServletRequest.class)).get().getAuthentication();
+
+        // 인증 안 된 사용자인 경우
+        if (authentication == null || !(authentication.getPrincipal() instanceof Member)) {
+            throw new TokenException(TokenErrorStatus.INVALID_TOKEN);
+        }
+
         return authentication.getPrincipal();
     }
 }
