6d50b09032b451f6a11be770d9123b936ad3f95f
m1nzez <90819894+m1nzez@users.noreply.github.com>
2025-07-06 01:50:19 +0900

    Revert ":sparkles: feat: 아이디어 반응 적용"

--- src/main/java/com/example/DongNaeJoGak/global/apiPayload/code/status/error/IdeaErrorStatus.java
+++ src/main/java/com/example/DongNaeJoGak/global/apiPayload/code/status/error/IdeaErrorStatus.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 public enum IdeaErrorStatus implements BaseErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "IDEA404", "아이디어를 찾지 못했습니다."),
-    NOT_SUPPORTED_REACTIONTYPE(HttpStatus.BAD_REQUEST, "IDEA405", "지원하지 않는 반응 타입입니다.")
     ;
 
     private final HttpStatus httpStatus;
