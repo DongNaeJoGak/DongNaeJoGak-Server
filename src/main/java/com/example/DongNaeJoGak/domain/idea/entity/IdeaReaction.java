03c17506476ec371da1b036c08e41ac79b71fd3f
hyungjun0312 <156741635+hyungjun0312@users.noreply.github.com>
2025-07-09 23:27:22 +0900

    마이페이지 구현

--- src/main/java/com/example/DongNaeJoGak/domain/idea/entity/IdeaReaction.java
+++ src/main/java/com/example/DongNaeJoGak/domain/idea/entity/IdeaReaction.java
@@ -2,21 +2,17 @@ package com.example.DongNaeJoGak.domain.idea.entity;
 
 import com.example.DongNaeJoGak.domain.idea.entity.enums.IdeaReactionType;
 import com.example.DongNaeJoGak.domain.member.entity.Member;
+import com.example.DongNaeJoGak.global.entity.BaseEntity; // ✅ BaseEntity import
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class) // Auditing 활성화
 @Table(name = "reaction")
-public class IdeaReaction {
+public class IdeaReaction extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,9 +29,4 @@ public class IdeaReaction {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "idea_id")
     private Idea idea;
-
-    // createdAt 필드 추가
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
 }
