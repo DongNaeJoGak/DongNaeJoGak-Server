7b4ed9e0ecc5dcf3202599370cd2fd40ec9c5742
hyungjun0312 <156741635+hyungjun0312@users.noreply.github.com>
2025-07-12 03:04:45 +0900

    feat: 스웨거 주소 설정

--- src/main/java/com/example/DongNaeJoGak/global/config/SwaggerConfig.java
+++ src/main/java/com/example/DongNaeJoGak/global/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +17,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://dongnaejogak.kro.kr")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(new Components().addSecuritySchemes("BearerAuth",
                         new SecurityScheme()
